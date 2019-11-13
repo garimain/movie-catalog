82210572dae1d1e345833c983411fce81d53fdb2
Garima Kapoor <garima.kapoor@ibm.com>
2019-11-12 21:05:26 -0600

    added docker file with corresponding properties

--- src/main/java/com/learning/service/moviecatalog/MovieCatalogApplication.java
+++ src/main/java/com/learning/service/moviecatalog/MovieCatalogApplication.java
@@ -3,6 +3,7 @@ package com.learning.service.moviecatalog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
@@ -15,6 +16,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @EnableCircuitBreaker
 @EnableHystrixDashboard
+@EnableEurekaClient
 public class MovieCatalogApplication {
 	
 
